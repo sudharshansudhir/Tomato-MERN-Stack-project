a9f9da0c5732d050899ded89cacd12a67e4a77b2
Sudharshan.R <sudhir.ssr7@gmail.com>
2025-08-10 11:36:30 +0530

    Updates FoodDisplay.jsx

--- FrontEnd/src/components/FoodDisplay/FoodDisplay.jsx
+++ FrontEnd/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,41 +1,44 @@
-import React, { useContext, useEffect, useState } from 'react'
-import './FoodDisplay.css'
-import { StoreContext } from '../../context/StoreContext'
-import Fooditem from '../Fooditem/Fooditem.jsx'
+import React, { useEffect, useState } from 'react';
+import './FoodDisplay.css';
+import Fooditem from '../Fooditem/Fooditem.jsx';
 import axios from "../../axios.jsx";
 
+const FoodDisplay = ({ category }) => {
+  const [menu_list, setmenu_list] = useState([]);
 
-const FoodDisplay = ({category}) => {
-
-    // const {food_list} = useContext(StoreContext)
-    const [menu_list,setmenu_list]=useState([])
   useEffect(() => {
-  const fetchMenu = async () => {
-    try {
-      const res = await axios.get("http://localhost:3000/pages/menu");
-      setmenu_list(res.data.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  fetchMenu();
-}, []);
-
- 
+    const fetchMenu = async () => {
+      try {
+        const res = await axios.get("/pages/menu");
+        setmenu_list(res.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMenu();
+  }, []);
 
   return (
     <div className='food-display' id='food-display'>
-        <h2>Top dishes near you</h2>
-        <div className="food-display-list">
-          {menu_list.map((item,index)=>{
-            
-             if(category==="All" || category===item.category)
-                 return <Fooditem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={`http://localhost:3000${item.image}`} />
-          })}
-
-        </div>
+      <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {menu_list.map((item, index) => {
+          if (category === "All" || category === item.category) {
+            return (
+              <Fooditem
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={`${axios.defaults.baseURL}${item.image}`}
+              />
+            );
+          }
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
