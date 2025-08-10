c1f18d4e7bfc01e9022a34c1f592ae0d508d097f
Sudharshan.R <sudhir.ssr7@gmail.com>
2025-08-10 11:44:46 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -37,7 +37,7 @@ app.get("/",(req,res)=>{
 
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
